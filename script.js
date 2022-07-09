5caadcf743eb3638c54b1f6f943defe46a940065
abdellah beliacine <abdellahbelyacine49@gmail.com>
2022-07-09 04:49:44 +0100

    fixed some typing errors

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
 function computerPlay(){
     return choices[Math.floor(Math.random()*3)];
 }
-function playRound(playerSelection,computerSelection){
-computerPlay();
-computerSelection=computerPlay();
-switch(playerSelection){
-    case "rock":
-        switch (computerSelection){
-            case "rock": return playRound(playerSelection,computerSelection); break;
-            case "scissors": return "you won , rock beats scissors"; break;
-            case "paper": return "you lose , paper beats rock";break;
-    }
-    case "paper":
-        switch (computerSelection){
-            case "paper": return playRound(playerSelection,computerSelection); break;
-            case "rock": return "you won , rock beats scissors"; break;
-            case "scissors": return "you lose , paper beats rock";break;
-        }    
-    case "scissors": 
-        switch (computerSelection){
-            case "scissors": return playRound(playerSelection,computerSelection); break;
-            case "paper": return "you won , rock beats scissors"; break;
-            case "rock": return "you lose , paper beats rock";break;
+function playRound(){
+let computerSelection=computerPlay();
+let playerSelection = window.prompt("what's your choice?").toLowerCase();
+if (choices.includes(playerSelection)){
+    switch(playerSelection){
+        case "rock":
+            switch (computerSelection){
+                case "rock": return playRound(); break;
+                case "scissors": return "you won , rock beats scissors"; break;
+                case "paper": return "you lose , paper beats rock";break;
         }
+        case "paper":
+            switch (computerSelection){
+                case "paper": return playRound(); break;
+                case "rock": return "you won , paper beats rock"; break;
+                case "scissors": return "you lose , scissors beats paper";break;
+            }    
+        case "scissors": 
+            switch (computerSelection){
+                case "scissors": return playRound(); break;
+                case "paper": return "you won , scissors beats paper"; break;
+                case "rock": return "you lose , rock beats scissors";break;
+            }
+    }
+}else{
+    playRound();
 }
 }
 function Game(){   
-    let winner=0;
+    let winnings=0;
     let key = "won";
     for (let i=0;i<5;i++){
-        computerPlay();
-        computerSelection=computerPlay();
-        let playerSelection = window.prompt("what's your choice?").toLowerCase();
-        if (choices.includes(playerSelection)){
-            playRound(playerSelection,computerSelection);
-            let result= playRound(playerSelection,computerSelection);
+            playRound();       
+            let result= playRound();
             if (result.split(' ').includes(key)){
-                winner++;    
+                winnings++;    
             }else{
-                winner--;
+                winnings--;
             }
-            console.log(result);
-        }else{
-            i--;
-        }     
+            console.log(result);   
 }
-    if (winner>2){
+    if (winnings>2){
             return "you won the game";
             }else{
             return "you've lost the game"
